f6217909d11cb9abddcf61bdadae81474f9ceb43
gwprdev <gwprdev@gmail.com>
2022-09-19 19:19:43 -0300

    Criando demais Elementos para Célula customizável

--- AppMusicBF/CustomView/CustomCardUIView.swift
+++ AppMusicBF/CustomView/CustomCardUIView.swift
@@ -22,7 +22,7 @@ class CustomCardUIView: UIView {
     var dataModel:CardViewModel?
 
     //MARK: ELEMENTOS
-    
+    //CardContainerView é o cara que vai receber todos os elementos.
     lazy var cardContainerView: UIView = {
         let v = UIView()
         v.translatesAutoresizingMaskIntoConstraints = false
@@ -86,4 +86,100 @@ class CustomCardUIView: UIView {
         
         return btn
     }()
+    
+    //categoria da musica
+    lazy var cardCategoryTitleLabel: UILabel = {
+    let l = UILabel()
+    l.translatesAutoresizingMaskIntoConstraints = false
+        l.font = UIFont.systemFont(ofSize: 12,weight: .bold)
+    l.textColor = .white
+    return l
+    }()
+    
+    //data categoria
+    lazy var cardCategoryDateLabel: UILabel = {
+        let l = UILabel()
+        l.translatesAutoresizingMaskIntoConstraints = false
+        l.font = UIFont.systemFont(ofSize: 11, weight: .regular)
+        l.textColor = .white
+        return l
+    }()
+    
+    //Título
+    lazy var cardTitleLabel: UILabel = {
+        let l = UILabel()
+        l.translatesAutoresizingMaskIntoConstraints = false
+        l.font = UIFont.systemFont(ofSize: 31, weight: .bold)
+        l.textAlignment = .center
+        l.textColor = .white
+        return l
+    }()
+    
+    //Título
+    lazy var cardDescriptionTitleLabel: UILabel = {
+        let l = UILabel()
+        l.translatesAutoresizingMaskIntoConstraints = false
+        l.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        l.textAlignment = .center
+        l.textColor = .white
+        l.numberOfLines = 0
+        return l
+    }()
+    
+    //Like e tempo
+    lazy var cardLikeTimeLabel: UILabel = {
+        let l = UILabel()
+        l.translatesAutoresizingMaskIntoConstraints = false
+
+        return l
+    }()
+    
+    init(viewMode:ViewMode, cardData:CardViewModel){
+        let frame = CGRect.zero
+        self.vmode = viewMode
+        self.dataModel = cardData
+        super.init(frame: frame)
+        self.addSubViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubViews(){
+        self.addSubview(self.cardContainerView)
+        self.cardContainerView.addSubview(self.cardImage)
+        self.cardContainerView.addSubview(self.overlayView)
+        
+        self.cardContainerView.addSubview(self.profileBorderView)
+        self.cardContainerView.addSubview(self.cardProfilePicture)
+        self.cardContainerView.addSubview(self.addProfileImageButton)
+        
+        self.cardContainerView.addSubview(self.cardCategoryTitleLabel)
+        self.cardContainerView.addSubview(self.cardCategoryDateLabel)
+        
+        self.cardContainerView.addSubview(self.cardTitleLabel)
+        self.cardContainerView.addSubview(self.cardLikeTimeLabel)
+        
+        self.cardContainerView.addSubview(self.cardDescriptionTitleLabel)
+        
+        self.updateLayout(for: vmode ?? .card)
+        
+    }
+    
+    private func updateLayout(for mode: ViewMode){
+        if mode == .full{
+            self.containerLeadingConstraints?.constant = 0
+            self.containerTopConstraints?.constant = 0
+            self.containerBottomConstraints?.constant = 0
+            self.containerTrailingConstraints?.constant = 0
+            self.cardDescriptionTitleLabel.isHidden = false
+        }else{
+            self.containerLeadingConstraints?.constant = 30
+            self.containerTopConstraints?.constant = 15
+            self.containerBottomConstraints?.constant = -30
+            self.containerTrailingConstraints?.constant = -15
+            self.cardDescriptionTitleLabel.isHidden = true
+        }
+    }
 }
