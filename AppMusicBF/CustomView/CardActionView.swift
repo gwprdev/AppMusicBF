85f83191cd8a6e6f8c52932a303214dff94dc46d
gwprdev <gwprdev@gmail.com>
2022-09-21 18:12:54 -0300

    14 - Criando botões para action View

--- AppMusicBF/CustomView/CardActionView.swift
+++ AppMusicBF/CustomView/CardActionView.swift
@@ -51,16 +51,87 @@ class CardActionView: UIView {
         let btn = UIButton()
         btn.translatesAutoresizingMaskIntoConstraints = false
         btn.backgroundColor = .white.withAlphaComponent(0.3)
+        btn.layer.cornerRadius = 22.5
+        btn.setBackgroundImage(UIImage(named: "download")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        btn.tintColor = .white
+        btn.clipsToBounds = true
         return btn
     }()
     
+    lazy var notInterestedButtom:UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.backgroundColor = .white.withAlphaComponent(0.3)
+        btn.layer.cornerRadius = 22.5
+        btn.setBackgroundImage(UIImage(named: "restrict")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        btn.tintColor = .white
+        btn.clipsToBounds = true
+        return btn
+    }()
+    
+    lazy var playButtom:UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.backgroundColor = .white
+        btn.layer.cornerRadius = 35
+        btn.setBackgroundImage(UIImage(named: "playBtn")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        btn.tintColor = .black
+        btn.clipsToBounds = true
+        return btn
+    }()
+    
+    lazy var likeButtom:UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.backgroundColor = .white.withAlphaComponent(0.3)
+        btn.layer.cornerRadius = 22.5
+        btn.setBackgroundImage(UIImage(named: "like")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        btn.tintColor = .white
+        btn.clipsToBounds = true
+        return btn
+    }()
+    
+    lazy var moreButtom:UIButton = {
+        let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.backgroundColor = .white.withAlphaComponent(0.3)
+        btn.layer.cornerRadius = 22.5
+        btn.setBackgroundImage(UIImage(named: "more")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        btn.tintColor = .white
+        btn.clipsToBounds = true
+        return btn
+    }()
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.setupViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-
+    private func setupViews(){
+        self.addSubview(self.stackView)
+        self.stackView.addArrangedSubview(self.notInterestedView)
+        self.stackView.addArrangedSubview(self.playView)
+        self.stackView.addArrangedSubview(self.likeView)
+        
+        self.downloadView.addSubview(self.downloadButtom)
+        self.notInterestedView.addSubview(self.notInterestedButtom)
+        self.playView.addSubview(self.playButtom)
+        self.likeView.addSubview(self.likeButtom)
+        self.moreView.addSubview(self.moreButtom)
+    }
+    
+    
+    private func setupConstraints(){
+        self.stackView.pin(to: self)
+        NSLayoutConstraint.activate([
+        
+        
+        ])
+    }
+    
 }
